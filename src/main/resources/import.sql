9bf9edf21efc8b372343393af0d76dd47be2b946
pauloflau <pauloflau@gmail.com>
2025-03-28 16:02:24 -0300

    Fix: Alteração na configuração de CORS para permitir acesso do Angular à API

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,6 +7,6 @@
 --INSERT INTO tb_fornecedor ( nome, cnpj) VALUES ( 'Fornecedor A', '11111111');
 
 -- Inserir Produtos
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'paulo@gmail.com', '123', now());
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'daiane', 'daiane@gmail.com', '123', now());
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'santosflau@gmail.com', '123', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'teste@gmail.com', '123', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'daiane', 'teste2@gmail.com', '123', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'teste3@gmail.com', '123', now());
