f28b46d31e9f10f678cc0a09daf035e0714ad9dd
pauloflau <pauloflau@gmail.com>
2025-03-31 10:57:34 -0300

    Fix: Alterado o import sql para ja enviar criptografado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,6 +7,6 @@
 --INSERT INTO tb_fornecedor ( nome, cnpj) VALUES ( 'Fornecedor A', '11111111');
 
 -- Inserir Produtos
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'teste@gmail.com', '123', now());
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'daiane', 'teste2@gmail.com', '123', now());
-INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'teste3@gmail.com', '123', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'paulo', 'teste@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'daiane', 'teste2@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', now());
+INSERT INTO tb_usuario ( nome, email, senha, datahora) VALUES ( 'mauricio', 'teste3@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', now());
